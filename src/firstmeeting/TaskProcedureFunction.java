4ef05a099098898274dc64552201746d157e28d7
daniar-17 <daniarnuramin1@gmail.com>
2023-05-15 09:02:48 +0700

    Update Task Procedure Function 02

--- src/firstmeeting/TaskProcedureFunction.java
+++ src/firstmeeting/TaskProcedureFunction.java
@@ -13,29 +13,51 @@ public class TaskProcedureFunction {
     public static void main(String[] args) {
         System.out.println("Learning Procedure and Function");
         System.out.println("-------------------------------");
-        int panjang = 3;
+        int panjang = 5;
         int lebar = 3;
-        int tinggi = 3;
-        int sisi = 3;
-        int phi = 3;
+        int tinggi = 4;
+        int sisi = 7;
+        double phi = 3.14;
         int jari = 4;
         
         //Volume Kubus
         double ResultVolKubus = VolumeKubus(sisi);
-        System.out.println("Result Volume Kubus : " + ResultVolKubus);
+        resultAll("Result Volume Kubus : ", ResultVolKubus);
         
         //Volume Kubus
         double ResultVolBalok = VolumeBalok(panjang, lebar, tinggi);
-        System.out.println("Result Volume Balok : " + ResultVolBalok);
+        resultAll("Result Volume Balok : ", ResultVolBalok);
+        
+        //Volume Kubus
+        double ResultVolBola = VolumeBola(phi, jari);
+        resultAll("Result Volume Bola : ", ResultVolBola);
+        
+        //Volume Pyramid
+        double ResultVolPyramid = VolumePyramid(panjang, lebar, tinggi);
+        resultAll("Result Volume Pyramid : ", ResultVolPyramid);
+    }
+    
+    private static void resultAll(String text, double volume) {
+        System.out.println(text + volume);
     }
     
     public static double VolumeKubus(int sisi){
-        double Luas = Math.pow(sisi, 3);
-        return Luas;
+        double Volume = Math.pow(sisi, 3);
+        return Volume;
     }
     
     public static double VolumeBalok(int p, int l, int t){
-        double Luas = p * l * t;
-        return Luas;
+        double Volume = p * l * t;
+        return Volume;
+    }
+    
+    public static double VolumeBola(double phi, int r){
+        double Volume = (4/3) * phi * (Math.pow(r, 3));
+        return Volume;
+    }
+    
+    public static double VolumePyramid(int p, int l, int t){
+        double Volume = (p * l * t) / 3;
+        return Volume;
     }
 }
